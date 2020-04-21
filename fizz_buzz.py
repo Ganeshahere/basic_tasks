2348fb5734cbf2489bba6b6fe4cbfd4ff937819e
Ganesha_here <Jamper1488@gmail.com>
2020-04-21 09:41:32 +0300

    1 task added, fizzbuzz refactored

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,15 +1,10 @@
+import prompt
+def t(num):
+    response = ('Fizz' if num % 3 == 0 else '') + ('Buzz' if num % 5 == 0 else '')
+    return str(num) if not response else response
+
 def fizz_buzz(start, stop):
-    result = ''
-
-    for x in range(start, stop + 1):
-        part_fizz = 'Fizz' if x % 3 == 0 else ''
-        part_buzz = 'Buzz' if x % 5 == 0 else ''
-        if part_fizz or part_buzz:
-            result += '{}{}' .format(part_fizz, part_buzz)
-        else:
-            result += str(x)
-        if x != stop:
-            result += ' '
-
-    return result
-print(fizz_buzz(1, 100))
+    return ' '.join(map(t, range(start, stop + 1)))
+start = prompt.integer('enter start: ')
+stop = prompt. integer('enter stop: ')
+print(fizz_buzz(start, stop))
